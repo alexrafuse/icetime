162b538ed3f7546111490431851f4706c910e687
Alex Rafuse <hey@alexrafuse.com>
2025-11-11 17:34:25 -0400

    fixes

---
+++ curlingio.ts
@@ -0,0 +1,145 @@
+
+export const curlingIoProducts = [
+  {
+    id: 9754,
+    name: "2025 Adult Learn to Curl Program:  Oct 20-Dec 15",
+    price: "$80.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9754-2025-adult-learn-to-curl-program-oct-20-dec-15/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  {
+    id: 9755,
+    name: "2025-2026 Green League First Half Oct-Dec 2025",
+    price: "$160.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9755-2025-2026-green-league-first-half-oct-dec-2025/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  
+  {
+    id: 9756,
+    name: "2025-2026 Green League Full Year October-March",
+    price: "$270.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9756-2025-2026-green-league-full-year-october-march/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  {
+    id: 9757,
+    name: "2025-2026 Membership:  Active",
+    price: "$550.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9757-2025-2026-membership-active/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  
+  {
+    id: 9758,
+    name: "2025-2026 Membership:  Active 2 Adults (same address)",
+    price: "$1,010.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9758-2025-2026-membership-active-2-adults-same-address/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  
+  {
+    id: 9761,
+    name: "2025-2026 Membership:  Half Year - Active",
+    price: "$355.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9761-2025-2026-membership-half-year-active/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  
+  {
+    id: 9763,
+    name: "2025-2026 Membership:  One Evening League Only",
+    price: "$400.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9763-2025-2026-membership-one-evening-league-only/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  
+  {
+    id: 9760,
+    name: "2025-2026 Membership:  New Member - Active",
+    price: "$455.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9760-2025-2026-membership-new-member-active/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+
+  {
+    id: 9759,
+    name: "2025-2026 Membership: New Members - Active 2 Adults (same address)",
+    price: "$810.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9759-2025-2026-membership-new-members-active-2-adults-same-address/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  {
+    id: 9762,
+    name: "2025-2026 Membership:  New Member Half Year - Active",
+    price: "$300.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9762-2025-2026-membership-new-member-half-year-active/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  {
+    id: 9764,
+    name: "2025-2026 Membership:  New Member One Evening League Only",
+    price: "$355.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9764-2025-2026-membership-new-member-one-evening-league-only/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  {
+    id: 9765,
+    name: "2025-2025 Membership:  Stick Curling League",
+    price: "$400.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9765-2025-2025-membership-stick-curling-league/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  {
+    id: 9766,
+    name: "2025-2026 Membership:  Student Curling in Evening League",
+    price: "$315.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9766-2025-2026-membership-student-curling-in-evening-league/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  {
+    id: 9767,
+    name: "2025-2026 Membership:  New Student Curling in Evening League",
+    price: "$250.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9767-2025-2026-membership-new-student-curling-in-evening-league/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  {
+    id: 9768, 
+    name: "2025-2026 Membership:  Social",
+    price: "$60.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9768-2025-2026-membership-social/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  
+  {
+    id: 9769,
+    name: "2025-2026 Locker Rental",
+    price: "$25.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9769-2025-2026-locker-rental/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+  
+  
+  {
+    id: 9770,
+    name: "2025-2026 Key Fob",
+    price: "$20.00",
+    add_to_cart_url: "https://bridgewater.curling.io/en/products/9770-2025-2026-key-fob/add_to_cart",
+    add_to_cart_text: "Add to cart",
+  },
+]
